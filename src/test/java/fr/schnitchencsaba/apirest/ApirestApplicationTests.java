164f89c2281bc7700fa0b04bcf07fb5b388cc83c
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-03 16:22:34 +0200

    feat: connection to database

--- src/test/java/fr/schnitchencsaba/apirest/ApirestApplicationTests.java
+++ src/test/java/fr/schnitchencsaba/apirest/ApirestApplicationTests.java
@@ -1,12 +1,20 @@
 package fr.schnitchencsaba.apirest;
 
+import fr.schnitchencsaba.apirest.feature.hello.HelloController;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,6 +25,11 @@ class ApirestApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Test
     void contextLoads() {
@@ -32,6 +45,15 @@ class ApirestApplicationTests {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
                 .andExpect(content().string("Hello World!"));
+    }
+
+    @Test
+    void testDatabase() {
+        Query query = entityManager.createNativeQuery("SHOW tables;");
+        List<String> results = ((List<String>) query.getResultList());
+        String resultList = String.join(" - ", results);
+        logger.info("Connection to the DB :: SUCCESS");
+        logger.info("List of tables: [{}]", resultList);
     }
 
 }
