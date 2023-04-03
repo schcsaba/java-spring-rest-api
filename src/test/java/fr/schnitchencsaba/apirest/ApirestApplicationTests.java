71e7fe4ab2b39de5186e2dcbf0a996b245ec2e60
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-03 10:18:07 +0200

    test: create first test

--- src/test/java/fr/schnitchencsaba/apirest/ApirestApplicationTests.java
+++ src/test/java/fr/schnitchencsaba/apirest/ApirestApplicationTests.java
@@ -1,13 +1,37 @@
 package fr.schnitchencsaba.apirest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApirestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void helloWorld() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/api/hello/world")
+                                .contentType(MediaType.TEXT_PLAIN_VALUE)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Hello World!"));
+    }
 
 }
